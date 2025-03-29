be6e0f32d0b6157ea3171e58167e994422e6f2ba
jdemum <jd37190@gmail.com>
2025-03-28 21:01:47 -0500

    added circleshape.py file, updated fps logic

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -17,7 +19,9 @@ def main():
                 return  # Exit the main() function and close the program
         screen.fill((0, 0, 0))
         pygame.display.flip()
-
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+             
 
 if __name__ == "__main__":
     main()
